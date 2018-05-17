f4b7d038981ee8054f737120f0018ca02076d446
EXADEL\edaugirdas <edaugirdas@exadel.com>
2018-05-17 11:42:22 +0300

    Add uniqid package, use it on forecast list

--- src/components/ForecastList/index.js
+++ src/components/ForecastList/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import uniqid from 'uniqid'
 
 import ForecastListItem from 'components/ForecastListItem'
 
 const ForecastList = ({ forecast }) => (
     <ul>
         {forecast.map(item => (
-            <ForecastListItem item={item} />
+            <ForecastListItem
+                key={uniqid()} 
+                item={item} 
+            />
         ))}
     </ul>
 )
